eb9495b0585457408dcde63f82ec657e00b7f087
Ruimei Li <rli11@cps.edu>
2019-07-17 10:57:33 -0500

    first commit

---
+++ myScript.js
@@ -0,0 +1,7 @@
+function showTime(){
+  document.getElementById('demo').innerHTML = Date();
+}
+function virus(){
+  document.getElementById('virus').innerHTML = window.alert("WARNING! YOUR COMPUTER IS INFECTED: System Detected (2) Potentially Malicious Viruses: Rootkit.Sirefef.Spy and Trojan.FakeAV-Download. Your Personal & Financial Information IS NOT SAFE."
+  );
+}
